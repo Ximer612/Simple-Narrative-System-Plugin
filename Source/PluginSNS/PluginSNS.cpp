7224fe5c4ee497918c19e4db0868ff6a75820154
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2024-07-20 16:34:09 +0200

    Init project

---
+++ Source/PluginSNS/PluginSNS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PluginSNS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PluginSNS, "PluginSNS" );
